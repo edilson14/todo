a8e7179a99465bc79541f4a53a6fd14bef77de11
edilson.tavares <edytavares66@gmail.com>
2025-02-04 22:34:15 -0300

    remove ITasksRepository and add use case classes for task management

--- lib/src/domain/IRepositorys/itask_repository.dart
+++
@@ -1,9 +0,0 @@
-import 'package:result_dart/result_dart.dart';
-import 'package:todo/src/domain/models/task_dto.dart';
-
-abstract class ITasksRepository {
-  AsyncResult<List<TaskDTO>> getTasks();
-  AsyncResult<TaskDTO> addTask(TaskDTO task);
-  AsyncResult<TaskDTO> updateTask(TaskDTO task);
-  AsyncResult<TaskDTO> deleteTask(TaskDTO task);
-}

e76a3e464c323b53fd2829b7558832cde202d181
edilson.tavares <edytavares66@gmail.com>
2025-02-10 20:37:24 -0300

    refator: renomiar a classe e pasta

---
+++ lib/src/domain/repositories/task_repository_interface.dart
@@ -0,0 +1,11 @@
+import 'package:result_dart/result_dart.dart';
+import 'package:todo/src/domain/dto/create_task_dto.dart';
+import 'package:todo/src/domain/dto/task_dto.dart';
+import 'package:todo/src/domain/entity/task_entity.dart';
+
+abstract class ITasksRepository {
+  AsyncResult<List<TaskEntity>> getTasks();
+  AsyncResult<Unit> addTask(CreateTaskDTO task);
+  AsyncResult<Unit> updateTask(TaskDTO task);
+  AsyncResult<Unit> deleteTask(TaskDTO task);
+}
--- lib/src/domain/repository/task_repository_interface.dart
+++
@@ -1,9 +0,0 @@
-import 'package:result_dart/result_dart.dart';
-import 'package:todo/src/domain/entity/task_entity.dart';
-
-abstract class ITasksRepository {
-  AsyncResult<List<TaskEntity>> getTasks();
-  AsyncResult<Unit> addTask(TaskEntity task);
-  AsyncResult<Unit> updateTask(TaskEntity task);
-  AsyncResult<Unit> deleteTask(TaskEntity task);
-}

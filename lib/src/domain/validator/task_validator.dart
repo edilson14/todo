936aab9854df3d79b6024978df73628bedf056d7
edilson.tavares <edytavares66@gmail.com>
2025-02-12 22:37:06 -0300

    fix: corrigir validator de task garantido que o nome e a descricao nao sejam vazios

--- lib/src/domain/validator/task_validator.dart
+++ lib/src/domain/validator/task_validator.dart
@@ -4,11 +4,11 @@ import 'package:todo/src/domain/dto/task_dto.dart';
 class TaskValidator extends LucidValidator<TaskDTO> {
   TaskValidator() {
     ruleFor((task) => task.name, key: 'name') //
-        .isEmpty(message: 'Nome nao pode ser vazio')
+        .notEmpty(message: 'Nome nao pode ser vazio')
         .minLength(3, message: 'Nome deve ter no minimo 3 caracteres');
 
     ruleFor((task) => task.description, key: 'description') //
-        .isEmpty(message: 'Descrição nao pode ser vazia')
+        .notEmpty(message: 'Descrição nao pode ser vazia')
         .minLength(10, message: 'Descrição deve ter no minimo 10 caracteres');
   }
 }

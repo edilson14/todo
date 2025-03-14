17b5140755caf40087207d39fd5f919e0f647ad3
edilson.tavares <edytavares66@gmail.com>
2025-03-14 20:33:57 -0100

    test: adicionar mock para o serviço de armazenamento local e implementar testes para o data source de tarefas

---
+++ test/mocks/local_storage_service_mock.dart
@@ -0,0 +1,5 @@
+import 'package:mocktail/mocktail.dart';
+import 'package:todo/src/external/local_storage_interface.dart';
+
+class LocalStorageServiceMock extends Mock
+    implements LocalStorageServiceInterface {}
--- test/src/data/data_source/task_datasource_test.dart
+++ test/src/data/data_source/task_datasource_test.dart
@@ -1,7 +1,43 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:result_dart/result_dart.dart';
+import 'package:todo/src/data/data_source/task_datasource.dart';
+import 'package:todo/src/external/local_storage_interface.dart';
+
+import '../../../mocks/local_storage_service_mock.dart';
 
 void main() {
-  testWidgets('task datasource ...', (tester) async {
-    // TODO: Implement test
-  });
+  late LocalStorageServiceInterface localStorageServiceInterface;
+  late TaskDataSource taskDataSource;
+  setUp(
+    () {
+      localStorageServiceInterface = LocalStorageServiceMock();
+
+      taskDataSource = TaskDataSource(localStorageServiceInterface);
+    },
+  );
+  group(
+    'Testando data source da tarefas',
+    () {
+      test(
+        'garantido o funcionamento do metodo de apagar a tarefa',
+        () async {
+          when(
+            () => localStorageServiceInterface.saveListItens(['1']),
+          ).thenAnswer(
+            (_) async => unit.toSuccess(),
+          );
+          when(() => localStorageServiceInterface.getListItens())
+              .thenAnswer((_) async => null);
+          await taskDataSource.deleteTask('1');
+
+          verifyNever(
+            () => localStorageServiceInterface.saveListItens(
+              ['tasks'],
+            ),
+          );
+        },
+      );
+    },
+  );
 }
